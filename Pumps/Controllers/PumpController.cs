f83dce190a881d26dc0ed9382a0362d0bb71bc66
DetiHoroniliKota <85434762+DetiHoroniliKota@users.noreply.github.com>
2023-04-01 17:23:36 +0300

    добавлен Контроллер, в setapProxy зарегистрированны пути.

---
+++ Pumps/Controllers/PumpController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Pumps.Interface;
+
+namespace Pumps.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PumpController : ControllerBase
+    {
+        private IPumpRepository _pumpRepository;
+
+        public PumpController(IPumpRepository pumpRepository)
+        {
+            _pumpRepository = pumpRepository;
+        }
+
+        [HttpGet]
+        public async Task<List<Pump.Models.Pump>> Get()
+        {
+            return await _pumpRepository.GetAllAsync();
+        }
+
+        [HttpPost("api/[controller]/createPump")]
+        public async Task Post()
+        {
+            var pump = new Pump.Models.Pump
+            {
+
+            };
+            await _pumpRepository.CrearAsync(pump);
+            
+
+        }
+
+    }
+}
